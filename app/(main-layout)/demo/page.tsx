c489e43ca79c82ad4c1b336ea278a126b9c8db75
haivn <haivn@guide.inc>
2025-03-31 12:53:14 +0700

    using react use kana

--- app/(main-layout)/demo/page.tsx
+++ app/(main-layout)/demo/page.tsx
@@ -1,15 +1,10 @@
 "use client";
 import { Flex, Input, Text, VStack } from "@chakra-ui/react";
-import { ChangeEventHandler, FunctionComponent, useState } from "react";
-import { toKatakana } from "wanakana";
+import { FunctionComponent } from "react";
+import { useKana } from "react-use-kana";
 
 const DemoKanjiToKana: FunctionComponent = () => {
-  const [enteredText, setEnteredText] = useState("");
-  const handleChangeKanji: ChangeEventHandler<HTMLInputElement> = (
-    textChanged
-  ) => {
-    setEnteredText(textChanged.target.value);
-  };
+  const { kana, setKanaSource } = useKana();
   return (
     <Flex
       flexDir="column"
@@ -22,12 +17,13 @@ const DemoKanjiToKana: FunctionComponent = () => {
         variant="outline"
         w="50%"
         px="1rem"
-        value={enteredText}
-        onChange={handleChangeKanji}
+        onChange={(e) => {
+          setKanaSource(e.target.value);
+        }}
       />
       <VStack w="50%" alignItems="start" mt="2rem">
         <Text>Converted Kana Below</Text>
-        <Text w="50%">{toKatakana(enteredText, {})}</Text>
+        <Text w="50%">{kana}</Text>
       </VStack>
     </Flex>
   );
